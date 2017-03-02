bec53fe83b0c7fd4ed1ad41559235996654b4e64
blazec <blaise@Blaises-MacBook-Pro.local>
2017-03-02 11:58:39 -0500

    Added intial files

---
+++ separatePunctuations.m
@@ -0,0 +1,24 @@
+function outSentence = separatePunctuations(inSentence)
+     % For both
+    %languages, separate sentence-final punctuation (sentences have already been determined for you), commas,
+    %colons and semicolons, parentheses, dashes between parentheses, mathematical operators (e.g., +, -, <,
+    %>, =), and quotation marks.
+    
+  outSentence = inSentence;
+  
+  outSentence = regexprep(outSentence, '\.', ' . ');
+  outSentence = regexprep(outSentence, '\?', ' ? ');
+  outSentence = regexprep(outSentence, '\!', ' ! ');
+  outSentence = regexprep(outSentence, '\,', ' , ');
+  outSentence = regexprep(outSentence, '\:', ' : ');
+  outSentence = regexprep(outSentence, '\;', ' ; ');
+  outSentence = regexprep(outSentence, '\(', ' ( ');
+  outSentence = regexprep(outSentence, '\)', ' ) ');
+  outSentence = regexprep(outSentence, '\-', ' - ');
+  outSentence = regexprep(outSentence, '\+', ' + ');
+  outSentence = regexprep(outSentence, ']\<', ' < ');
+  outSentence = regexprep(outSentence, '\>', ' > ');
+  outSentence = regexprep(outSentence, '\=', ' = ');
+  outSentence = regexprep(outSentence, ' +', ' ');
+
+end
